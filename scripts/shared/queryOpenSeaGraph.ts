deaa29d3b67dec67ee16832aa7cad6d6c20e21f5
Kris Urbas <krzysu@interia.pl>
2021-08-11 12:07:09 +0200

    fix graphql call

--- scripts/shared/queryOpenSeaGraph.ts
+++ scripts/shared/queryOpenSeaGraph.ts
@@ -93,6 +93,8 @@ const fetchCollectionForCursor = async (vars: QueryVars, cursor: string) => {
       headers: {
         "Content-Type": "application/json",
         "x-api-key": process.env.OPEN_SEA_API_KEY || "",
+        "user-agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
       },
       body: JSON.stringify({ query: QUERY, variables }),
     });
