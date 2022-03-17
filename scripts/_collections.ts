5b130e23eb005139c7ae69494fc63c60e14a32c3
Kris Urbas <krzysu@interia.pl>
2022-03-17 11:40:15 +0100

    add more collections

---
+++ scripts/_collections.ts
@@ -0,0 +1,38 @@
+export type Collection = {
+  name: string;
+  contractAddress: string;
+  totalSupply: number;
+};
+
+export const collections: Collection[] = [
+  {
+    name: "The Wicked Craniums",
+    contractAddress: "0x85f740958906b317de6ed79663012859067e745b".toLowerCase(),
+    totalSupply: 10762,
+  },
+  {
+    name: "Pixlton Car Club",
+    contractAddress: "0x584292974026978586c3007b5a15b69118130bbb".toLowerCase(),
+    totalSupply: 4006,
+  },
+  {
+    name: "Pixls",
+    contractAddress: "0x082903f4e94c5e10A2B116a4284940a36AFAEd63".toLowerCase(),
+    totalSupply: 5471,
+  },
+  {
+    name: "Hashmasks",
+    contractAddress: "0xc2c747e0f7004f9e8817db2ca4997657a7746928".toLowerCase(),
+    totalSupply: 16384,
+  },
+  {
+    name: "The Blitnauts",
+    contractAddress: "0x448f3219cf2a23b0527a7a0158e7264b87f635db".toLowerCase(),
+    totalSupply: 1484,
+  },
+  {
+    name: "MirrorWorld",
+    contractAddress: "0x7592e2f251a7f7da27211625d652092769f43a60".toLowerCase(),
+    totalSupply: 6580,
+  },
+];
--- scripts/_prices.ts
+++ scripts/_prices.ts
@@ -7,35 +7,7 @@ import {
 } from "./shared/database";
 import { queryOpenSeaAndSaveToDb } from "./shared/queryOpenSeaAndSaveToDb";
 import { DbPricedItem } from "./shared/types";
-
-type Collection = {
-  name: string;
-  contractAddress: string;
-  totalSupply: number;
-};
-
-const collections: Collection[] = [
-  // {
-  //   name: "The Wicked Craniums",
-  //   contractAddress: "0x85f740958906b317de6ed79663012859067e745b".toLowerCase(),
-  //   totalSupply: 10762,
-  // },
-  {
-    name: "Pixlton Car Club",
-    contractAddress: "0x584292974026978586c3007b5a15b69118130bbb".toLowerCase(),
-    totalSupply: 4006,
-  },
-  {
-    name: "Pixls",
-    contractAddress: "0x082903f4e94c5e10A2B116a4284940a36AFAEd63".toLowerCase(),
-    totalSupply: 5471,
-  },
-  // {
-  //   name: "Hashmasks",
-  //   contractAddress: "0xc2c747e0f7004f9e8817db2ca4997657a7746928".toLowerCase(),
-  //   totalSupply: 16384,
-  // },
-];
+import { Collection, collections } from "./_collections";
 
 const OS_PAGE_LIMIT = 50;
 
@@ -61,14 +33,14 @@ const getPricesForCollection = async (collection: Collection) => {
     // }),
   ]);
 
-  if (requiresExtraOpenSeaRound) {
-    await queryOpenSeaAndSaveToDb({
-      collectionAddress: collection.contractAddress,
-      order: "desc",
-      totalPages: halfOsPagesLength,
-      saveItems: async (items: DbPricedItem[]) => await saveToDb(items),
-    });
-  }
+  // if (requiresExtraOpenSeaRound) {
+  //   await queryOpenSeaAndSaveToDb({
+  //     collectionAddress: collection.contractAddress,
+  //     order: "desc",
+  //     totalPages: halfOsPagesLength,
+  //     saveItems: async (items: DbPricedItem[]) => await saveToDb(items),
+  //   });
+  // }
 };
 
 const main = async () => {
