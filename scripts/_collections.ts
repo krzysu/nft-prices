93f4603b50d6cd23c3c36ae70a9bcb0e754d6422
Kris Urbas <krzysu@interia.pl>
2022-04-28 12:27:50 +0200

    add new collections

--- scripts/_collections.ts
+++ scripts/_collections.ts
@@ -18,9 +18,7 @@ export const collections: Collection[] = [
     "The Wicked Craniums",
     "0x85f740958906b317de6ed79663012859067e745b"
   ),
-  // buildCollection("Hashmasks", "0xc2c747e0f7004f9e8817db2ca4997657a7746928"),
-  // buildCollection("Blitnauts", "0x448f3219cf2a23b0527a7a0158e7264b87f635db"),
-  // buildCollection("MirrorWorld", "0x7592e2f251a7f7da27211625d652092769f43a60"),
+  buildCollection("Blitnauts", "0x448f3219cf2a23b0527a7a0158e7264b87f635db"),
   buildCollection(
     "Crypto Corgis",
     "0x51e613727fdd2e0B91b51c3E5427E9440a7957E4"
@@ -29,8 +27,8 @@ export const collections: Collection[] = [
     "Bored Ape Yacht Club",
     "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"
   ),
-  buildCollection(
-    "Dusk by GEN.ART",
-    "0xdc1b41492db3b485914d92a2e2cfa35fd93fc8ca"
-  ),
+  buildCollection("Moonbirds", "0x23581767a106ae21c074b2276D25e5C3e136a68b"),
+  buildCollection("Azuki", "0xED5AF388653567Af2F388E6224dC7C4b3241C544"),
+  buildCollection("Arcade Land", "0x4a8c9d751eeabc5521a68fb080dd7e72e46462af"),
+  buildCollection("Hashmasks", "0xc2c747e0f7004f9e8817db2ca4997657a7746928"),
 ];
